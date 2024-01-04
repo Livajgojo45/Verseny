55c3f54777e17c05edd1b1870d7980b400194bc1
GergoWasTaken <magyar.gergo@kreativszolnok.hu>
2024-01-04 12:45:32 +0100

    További információk gomb, js-css

---
+++ js/info.js
@@ -0,0 +1,29 @@
+function elso_negyzet() {
+    alert("TESTMAGASSÁG: A testmagasságot a függőleges falfelület mellett, azt sarkával, tomporával és hátával érintő,álló személyen mérjük. A fejet úgy tartja, hogy a fülnyílás felső határa és a szemzug alsórésze egy egyenesbe esik, a sarka a lapon összeér. A vizsgált személy vegyen mély lélegzetetés azt tartsa meg, amíg centiméter pontossággal mérjük.")
+    alert("TESTTÖMEG: A minimális ruházatot viselő személy a mérleg közepére áll.")
+}
+
+function masodik_negyzet() {
+    alert("BOKAKERÜLET: A lábszár legkisebb kerülete.")
+    alert("CSUKLÓKERÜLET: Az alkar legkisebb kerülete.")
+}
+
+function harmadik_negyzet() {
+    alert("TRICEPSZREDŐ: A méretet a vizsgált személy lazán lógó karjának hátsó oldalán, a vállcsúcsot a könyökkel összekötő vonal képzeletbeli felezőpontjánál vesszük fel.")
+    alert("LAPOCKAREDŐ: A lapocka alatti bőrredőhöz a redőt a lapocka belső, ferde vonalánál, lefelé és kifelé 45 fokos szögben emeljük fel.")
+    alert("COMBREDŐ: Mérjük meg a térdkalács felett, és a lágyék vonala között, középtájon, a függőlegesen összecsípett bőrt.")
+    alert("KÖLDÖKREDŐ: Körülbelül 2 cm-re a köldöktől, függőlegesen kihúzott bőrön végezzük.")
+    alert("ALSZÁRREDŐ: A lábszáron a legnagyobb kerület magasságában függőleges redő.")
+}
+
+function negyedik_negyzet() {
+    alert("NYÚJTOTT FELKARKERÜLET: A felkar nyújtott, a test mellett lógó állapotban mért legnagyobb kerülete.")
+    alert("ALKARKERÜLET: Az alkar legnagyobb kerülete.")
+    alert("MELLKASKERÜLET: Hátul a lapocka alsó csúcsánál, elöl a melbimbók felett vezetett mérőszalaggal mérve – nyugalmi kilégzés végén.")
+    alert("COMBKERÜLET: A fenék alatt területen mért kerület.")
+    alert("ALSZÁRKERÜLET: A nyújtott lábszár legnagyobb kerülete.")
+    alert("MELLKASSZÉLESSÉG: A leginkább kidomborodó bordapontok vízszintes távolsága légzési középállás helyzetében.")
+    alert("VÁLLSZÉLESSÉG: Vállcsúcsok távolsága.")
+    alert("CSÍPŐSZÉLESSÉG: A csípő két legtávolabbi pontja között mért távolság.")
+    alert("MELLKASMÉLYSÉG: A szegycsont és a vele egy magasságban lévő hátcsigolya távolsága légzési középállapotban.")
+}
