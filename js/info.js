495e89450bc62331c4d1fb759bb7edb177856178
GergoWasTaken <magyar.gergo@kreativszolnok.hu>
2024-01-04 12:54:34 +0100

    további infó

--- js/info.js
+++ js/info.js
@@ -1,29 +1,15 @@
 function elso_negyzet() {
-    alert("TESTMAGASSÁG: A testmagasságot a függőleges falfelület mellett, azt sarkával, tomporával és hátával érintő,álló személyen mérjük. A fejet úgy tartja, hogy a fülnyílás felső határa és a szemzug alsórésze egy egyenesbe esik, a sarka a lapon összeér. A vizsgált személy vegyen mély lélegzetetés azt tartsa meg, amíg centiméter pontossággal mérjük.")
-    alert("TESTTÖMEG: A minimális ruházatot viselő személy a mérleg közepére áll.")
+    alert("TESTMAGASSÁG: A testmagasságot a függőleges falfelület mellett, azt sarkával, tomporával és hátával érintő,álló személyen mérjük. A fejet úgy tartja, hogy a fülnyílás felső határa és a szemzug alsórésze egy egyenesbe esik, a sarka a lapon összeér. A vizsgált személy vegyen mély lélegzetetés azt tartsa meg, amíg centiméter pontossággal mérjük.\nTESTTÖMEG: A minimális ruházatot viselő személy a mérleg közepére áll.")
 }
 
 function masodik_negyzet() {
-    alert("BOKAKERÜLET: A lábszár legkisebb kerülete.")
-    alert("CSUKLÓKERÜLET: Az alkar legkisebb kerülete.")
+    alert("BOKAKERÜLET: A lábszár legkisebb kerülete.\nCSUKLÓKERÜLET: Az alkar legkisebb kerülete.")
 }
 
 function harmadik_negyzet() {
-    alert("TRICEPSZREDŐ: A méretet a vizsgált személy lazán lógó karjának hátsó oldalán, a vállcsúcsot a könyökkel összekötő vonal képzeletbeli felezőpontjánál vesszük fel.")
-    alert("LAPOCKAREDŐ: A lapocka alatti bőrredőhöz a redőt a lapocka belső, ferde vonalánál, lefelé és kifelé 45 fokos szögben emeljük fel.")
-    alert("COMBREDŐ: Mérjük meg a térdkalács felett, és a lágyék vonala között, középtájon, a függőlegesen összecsípett bőrt.")
-    alert("KÖLDÖKREDŐ: Körülbelül 2 cm-re a köldöktől, függőlegesen kihúzott bőrön végezzük.")
-    alert("ALSZÁRREDŐ: A lábszáron a legnagyobb kerület magasságában függőleges redő.")
+    alert("TRICEPSZREDŐ: A méretet a vizsgált személy lazán lógó karjának hátsó oldalán, a vállcsúcsot a könyökkel összekötő vonal képzeletbeli felezőpontjánál vesszük fel.\nLAPOCKAREDŐ: A lapocka alatti bőrredőhöz a redőt a lapocka belső, ferde vonalánál, lefelé és kifelé 45 fokos szögben emeljük fel.\nCOMBREDŐ: Mérjük meg a térdkalács felett, és a lágyék vonala között, középtájon, a függőlegesen összecsípett bőrt.\nKÖLDÖKREDŐ: Körülbelül 2 cm-re a köldöktől, függőlegesen kihúzott bőrön végezzük.\nALSZÁRREDŐ: A lábszáron a legnagyobb kerület magasságában függőleges redő.")
 }
 
 function negyedik_negyzet() {
-    alert("NYÚJTOTT FELKARKERÜLET: A felkar nyújtott, a test mellett lógó állapotban mért legnagyobb kerülete.")
-    alert("ALKARKERÜLET: Az alkar legnagyobb kerülete.")
-    alert("MELLKASKERÜLET: Hátul a lapocka alsó csúcsánál, elöl a melbimbók felett vezetett mérőszalaggal mérve – nyugalmi kilégzés végén.")
-    alert("COMBKERÜLET: A fenék alatt területen mért kerület.")
-    alert("ALSZÁRKERÜLET: A nyújtott lábszár legnagyobb kerülete.")
-    alert("MELLKASSZÉLESSÉG: A leginkább kidomborodó bordapontok vízszintes távolsága légzési középállás helyzetében.")
-    alert("VÁLLSZÉLESSÉG: Vállcsúcsok távolsága.")
-    alert("CSÍPŐSZÉLESSÉG: A csípő két legtávolabbi pontja között mért távolság.")
-    alert("MELLKASMÉLYSÉG: A szegycsont és a vele egy magasságban lévő hátcsigolya távolsága légzési középállapotban.")
+    alert("NYÚJTOTT FELKARKERÜLET: A felkar nyújtott, a test mellett lógó állapotban mért legnagyobb kerülete.\nALKARKERÜLET: Az alkar legnagyobb kerülete.\nMELLKASKERÜLET: Hátul a lapocka alsó csúcsánál, elöl a melbimbók felett vezetett mérőszalaggal mérve – nyugalmi kilégzés végén.\nCOMBKERÜLET: A fenék alatt területen mért kerület.\nALSZÁRKERÜLET: A nyújtott lábszár legnagyobb kerülete.\nMELLKASSZÉLESSÉG: A leginkább kidomborodó bordapontok vízszintes távolsága légzési középállás helyzetében.\nVÁLLSZÉLESSÉG: Vállcsúcsok távolsága.\nCSÍPŐSZÉLESSÉG: A csípő két legtávolabbi pontja között mért távolság.\nMELLKASMÉLYSÉG: A szegycsont és a vele egy magasságban lévő hátcsigolya távolsága légzési középállapotban.")
 }
